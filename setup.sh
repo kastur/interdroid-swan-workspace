098165ebef7f562ca43f61f75e6b13fa52b10f73
Nick Palmer <nick@sluggardy.net>
2011-11-24 17:14:44 +0100

    Be a little more defensive with return codes and existing directories.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 git submodule update --init
+
+if [ $? != 0 ] ; then
+    echo "Unable to run git submodule update --init"
+    exit 1
+fi
+
 for i in `cat android-subprojects`
 do
-    mkdir $i/gen
+    if [ ! -d $i/gen ] ; then
+	mkdir $i/gen || die unable to make gen directory
+    fi
 done
